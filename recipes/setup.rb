ceb4d21934105556c32c24c75d73d765556b06c7
Cloud User <centos@ip-172-31-1-78.us-west-1.compute.internal>
2018-02-26 17:35:19 +0000

    Added system info to motd

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -16,7 +16,12 @@ end
 
 file '/etc/motd' do
    action :create
-   content 'This server is the property of Josh'
+   content "This server is the property of Josh
+   HOSTNAME: #{node['hostname']}
+   IPADDRESS: #{node['ipaddress']}
+   CPU: #{node['cpu']['0']['mhz']}
+   MEMORY: #{node['memory']['total']}
+"
    group 'root'
    owner 'root'
 end
